dc7fcfb5810d8ef7bcdae56d335d2c789b3976ef
rebeccali <rebecca.li@kittyhawk.aero>
2018-01-15 18:51:57 -0800

    LINTING

--- places365.py
+++ places365.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
-# PlacesCNN to predict the scene category, attribute, and class activation map in a single pass
-# by Bolei Zhou, sep 2, 2017
-
+'''
+    PlacesCNN to predict the scene category, attribute, and class activation map in a single pass
+    by Bolei Zhou, sep 2, 2017
+'''
 
 import torch
 from torch.autograd import Variable as V
+from torch.nn import functional as F
+
 import torchvision.models as models
 from torchvision import transforms as trn
-from torch.nn import functional as F
+
 import os
 import numpy as np
 from scipy.misc import imresize as imresize
@@ -16,24 +19,111 @@ import cv2
 from PIL import Image
 import sys
 
-# imgpath is the path to the image to be processed
-# render_cam is to generate a cam image
-# output: environment (string), category probabilities (float, array), categories (string, array), attributes (string)
-class Place365(object):
+
+def image_var_laplacian(img):
+    """calculates laplacian variance of image"""
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return cv2.Laplacian(gray, cv2.CV_64F).var()
+
+def image_var_canny(img):
+    """calculates canny edges variance"""
+    return cv2.Canny(img,100,200).var()
+
+def load_labels():
+    """load places365 labels"""
+    # prepare all the labels
+    # scene category relevant
+    file_name_category = 'categories_places365.txt'
+    if not os.access(file_name_category, os.W_OK):
+        print('categories_places365.txt not found!')
+        sys.exit(1)
+    classes = list()
+    with open(file_name_category) as class_file:
+        for line in class_file:
+            classes.append(line.strip().split(' ')[0][3:])
+    classes = tuple(classes)
+
+    # indoor and outdoor relevant
+    file_name_IO = 'IO_places365.txt'
+    if not os.access(file_name_IO, os.W_OK):
+        print('IO_places365.txt not found!')
+        sys.exit(1)
+    with open(file_name_IO) as f:
+        lines = f.readlines()
+        labels_IO = []
+        for line in lines:
+            items = line.rstrip().split()
+            labels_IO.append(int(items[-1]) -1) # 0 is indoor, 1 is outdoor
+    labels_IO = np.array(labels_IO)
+
+    # scene attribute relevant
+    file_name_attribute = 'labels_sunattribute.txt'
+    if not os.access(file_name_attribute, os.W_OK):
+        print('labels_sunattribute.txt not found!')
+        sys.exit(1)
+    with open(file_name_attribute) as f:
+        lines = f.readlines()
+        labels_attribute = [item.rstrip() for item in lines]
+    file_name_W = 'W_sceneattribute_wideresnet18.npy'
+    if not os.access(file_name_W, os.W_OK):
+        print('W_sceneattribute_wideresnet18.npy not found!')
+        sys.exit(1)
+    W_attribute = np.load(file_name_W)
+
+    return classes, labels_IO, labels_attribute, W_attribute
+
+def returnCAM(feature_conv, weight_softmax, class_idx):
+    """create cam image"""
+    # generate the class activation maps upsample to 256x256
+    size_upsample = (256, 256)
+    nc, h, w = feature_conv.shape
+    output_cam = []
+    for _ in class_idx:
+        cam = weight_softmax[class_idx].dot(feature_conv.reshape((nc, h*w)))
+        cam = cam.reshape(h, w)
+        cam = cam - np.min(cam)
+        cam_img = cam / np.max(cam)
+        cam_img = np.uint8(255 * cam_img)
+        output_cam.append(imresize(cam_img, size_upsample))
+    return output_cam
+
+def returnTF():
+    """load the image transformer"""
+    tf = trn.Compose([
+        trn.Resize((224, 224)),
+        trn.ToTensor(),
+        trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    return tf
+
+def compute_blurriness(imgpath):
+    """ computes image blurriness using canny"""
+
+    img_cv2 = cv2.imread(imgpath)
+    # bluriness = self.image_var_laplacian(img_cv2)
+    bluriness = image_var_canny(img_cv2)
+    return bluriness
+
+
+class TaggedImage(object):
+    '''
+    imgpath is the path to the image to be processed
+    render_cam is to generate a cam image
+    output: environment (string), category probabilities (float, array),
+    categories (string, array), attributes (string)
+    '''
     features_blobs = []
 
-    def image_var_laplacian(self, img):
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        return cv2.Laplacian(gray, cv2.CV_64F).var()
-
-    def image_var_canny(self, img):
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        return cv2.Canny(img,100,200).var()
+    def get_imgpath(self):
+        """gets imgpath"""
+        return self._imgpath
 
     def hook_feature(self, module, input, output):
+        """hooks feature for register hook"""
         self.features_blobs.append(np.squeeze(output.data.cpu().numpy()))
 
     def print_identification(self):
+        """prints out information on Places365"""
         print('----------------------------------------')
 
         print(self._imgpath)
@@ -48,77 +138,8 @@ class Place365(object):
         print('--SCENE ATTRIBUTES:')
         print(', '.join(self._attributes))
 
-    def load_labels(self):
-        # prepare all the labels
-        # scene category relevant
-        file_name_category = 'categories_places365.txt'
-        if not os.access(file_name_category, os.W_OK):
-            print('categories_places365.txt not found!')
-            sys.exit(1)
-        classes = list()
-        with open(file_name_category) as class_file:
-            for line in class_file:
-                classes.append(line.strip().split(' ')[0][3:])
-        classes = tuple(classes)
-
-        # indoor and outdoor relevant
-        file_name_IO = 'IO_places365.txt'
-        if not os.access(file_name_IO, os.W_OK):
-            print('IO_places365.txt not found!')
-            sys.exit(1)
-        with open(file_name_IO) as f:
-            lines = f.readlines()
-            labels_IO = []
-            for line in lines:
-                items = line.rstrip().split()
-                labels_IO.append(int(items[-1]) -1) # 0 is indoor, 1 is outdoor
-        labels_IO = np.array(labels_IO)
-
-        # scene attribute relevant
-        file_name_attribute = 'labels_sunattribute.txt'
-        if not os.access(file_name_attribute, os.W_OK):
-            print('labels_sunattribute.txt not found!')
-            sys.exit(1)
-        with open(file_name_attribute) as f:
-            lines = f.readlines()
-            labels_attribute = [item.rstrip() for item in lines]
-        file_name_W = 'W_sceneattribute_wideresnet18.npy'
-        if not os.access(file_name_W, os.W_OK):
-            print('W_sceneattribute_wideresnet18.npy not found!')
-            sys.exit(1)
-        W_attribute = np.load(file_name_W)
-
-        return classes, labels_IO, labels_attribute, W_attribute
-
-    def returnCAM(self, feature_conv, weight_softmax, class_idx):
-        # generate the class activation maps upsample to 256x256
-        size_upsample = (256, 256)
-        nc, h, w = feature_conv.shape
-        output_cam = []
-        for idx in class_idx:
-            cam = weight_softmax[class_idx].dot(feature_conv.reshape((nc, h*w)))
-            cam = cam.reshape(h, w)
-            cam = cam - np.min(cam)
-            cam_img = cam / np.max(cam)
-            cam_img = np.uint8(255 * cam_img)
-            output_cam.append(imresize(cam_img, size_upsample))
-        return output_cam
-
-    def returnTF(self):
-    # load the image transformer
-        tf = trn.Compose([
-            trn.Resize((224,224)),
-            trn.ToTensor(),
-            trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
-        return tf
-    def compute_blurriness(self, imgpath):
-        img_cv2 = cv2.imread(imgpath)
-        # bluriness = self.image_var_laplacian(img_cv2)
-        bluriness = self.image_var_canny(img_cv2)
-        return bluriness
-
     def load_model(self):
+        """Load places365 model """
         # this model has a last conv feature map as 14x14
 
         model_file = 'whole_wideresnet18_places365_python36.pth.tar'
@@ -133,7 +154,8 @@ class Place365(object):
 
         # the following is deprecated, everything is migrated to python36
 
-        ## if you encounter the UnicodeDecodeError when use python3 to load the model, add the following line will fix it. Thanks to @soravux
+        ## if you encounter the UnicodeDecodeError when use python3 to load the model,
+        # add the following line will fix it. Thanks to @soravux
         #from functools import partial
         #import pickle
         #pickle.load = partial(pickle.load, encoding="latin1")
@@ -142,21 +164,21 @@ class Place365(object):
 
         model.eval()
         # hook the feature extractor
-        features_names = ['layer4','avgpool'] # this is the last conv layer of the resnet
+        features_names = ['layer4', 'avgpool'] # this is the last conv layer of the resnet
         for name in features_names:
             model._modules.get(name).register_forward_hook(self.hook_feature)
         return model
 
     def __init__(self, imgpath, render_cam=False):
         # load the labels
-        classes, labels_IO, labels_attribute, W_attribute = self.load_labels()
+        classes, labels_IO, labels_attribute, W_attribute = load_labels()
 
         # self._classes = classes
         # load the model
         model = self.load_model()
 
         # load the transformer
-        tf = self.returnTF() # image transformer
+        tf = returnTF() # image transformer
 
         # get the softmax weight
         params = list(model.parameters())
@@ -190,7 +212,7 @@ class Place365(object):
 
         if render_cam:
             # generate class activation mapping
-            CAMs = self.returnCAM(self.features_blobs[0], weight_softmax, [idx[0]])
+            CAMs = returnCAM(self.features_blobs[0], weight_softmax, [idx[0]])
 
             # render the CAM and output
             img = cv2.imread(imgpath)
@@ -200,9 +222,9 @@ class Place365(object):
             cv2.imwrite('cam.jpg', result)
 
         self._imgpath = imgpath
-        self._bluriness = self.compute_blurriness(imgpath)
+        self._bluriness = compute_blurriness(imgpath)
         self._probs = probs[:5]
         self._idx = probs[:5]
         self._env = scene_env
         self._categories = [classes[x] for x in idx[:5]]
-        self._attributes = [labels_attribute[idx_a[i]] for i in range(-1,-10,-1)]
+        self._attributes = [labels_attribute[idx_a[i]] for i in range(-1, -10, -1)]
--- run_tagger.py
+++ run_tagger.py
@@ -4,22 +4,20 @@
 '''
 
 import argparse
-import sys
 import os
 import glob
 from datetime import datetime
 
 import places365 as pl
-import tagger
 
 parser = argparse.ArgumentParser(description='Run image tagger.')
 parser.add_argument('path', type=str, help='input image file or folder')
 parser.add_argument('--stfu', help='stfu', action='store_true', default=False)
-parser.add_argument('--names-only', help='print filenames only', default=False, action='store_true' )
+parser.add_argument('--names-only', help='print filenames only', default=False, action='store_true')
 
 args = parser.parse_args()
 
-acceptedFileExtentions = ['jpg','png', 'tif', 'dng', 'nef' ]
+acceptedFileExtentions = ['jpg', 'png', 'tif', 'dng', 'nef']
 
 # get files in directory
 if os.path.isfile(args.path):
@@ -39,15 +37,14 @@ num_images = len(paths)
 
 if num_images == 0:
     raise Exception(args.path + ' contains no pictures. Accepted filetypes are '
-        + ','.join(acceptedFileExtentions) + '.')
-
+                    + ','.join(acceptedFileExtentions) + '.')
 
 startTime = datetime.now()
 
 for path in paths:
-    imgplaces = pl.Place365(path, True)
+    imgplaces = pl.TaggedImage(path, True)
     if args.names_only:
-        print(imgplaces._imgpath)
+        print(imgplaces.get_imgpath)
     elif not args.stfu:
         imgplaces.print_identification()
 
@@ -55,6 +52,5 @@ elapsed_time = (datetime.now() - startTime).total_seconds()
 elapsed_time_per_image = elapsed_time / num_images
 
 if not args.stfu:
-    print('Process time: %0.2f seconds' % elapsed_time )
-    print('Process time per image: %0.2f milliseconds' % elapsed_time_per_image )
-
+    print('Process time: %0.2f seconds' % elapsed_time)
+    print('Process time per image: %0.2f milliseconds' % elapsed_time_per_image)
